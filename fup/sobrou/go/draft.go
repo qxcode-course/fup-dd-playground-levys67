b12854c0c437e3e62c24149b06c3ac1bcfcb98c7
levys67 <naoeolevy@gmail.com>
2026-03-24 18:05:27 +0000

    coisas

--- fup/sobrou/go/draft.go
+++ fup/sobrou/go/draft.go
@@ -1,17 +1,16 @@
-package main
+ package main
 import "fmt"
 func main() {
-    var a float32 = 2
-    var b float32 = 8
-    var c float32 = 5
+    var a float64 
+    var b float64 
+    var c float64 
+    var z float64 
+    var x float64 
+    var y float64 
     fmt.Scan(&a, &b, &c)
-    var z float32 = 1555
-    var x float32 = 4206
-    var y float32 = 761
-    fmt.Scan(&z, &x, &y)
-    var din float32 = 54771.81
-    var az float32 = a * z
-    var bx float32 = b * x
-    
-    fmt.Println("qxcode")
+    fmt.Scan(&z, &x, &y) 
+    var din float64  
+    fmt.Scan(&din)
+    var gas float64 = (a * z) + (b * x) + (c * y)
+    fmt.Printf("%.2f\n",din - gas)
 }
--- sandbox/00_rascunhos/bannada/draft.go
+++ sandbox/00_rascunhos/bannada/draft.go
@@ -1,10 +1,16 @@
 package main
 import "fmt"
 func main() {
-    var a int = 2
-    var b int = 3
-    fmt.Scan(&a, &b)
-    var div int = a / b 
-    var res int = a - b
-    fmt.Print(div, res)
+    var a float32 = 2
+    var b float32 = 8
+    var c float32 = 5
+    fmt.Scan(&a, &b, &c)
+    var z float32 = 1555
+    var x float32 = 4206
+    var y float32 = 761
+    fmt.Scan(&z, &x, &y)
+    var din float32 = 54771.87
+    var gas float32 = a * z + b * x + c * y
+    fmt.Scan(&din, &gas)
+    fmt.Println(din - gas)
 }
