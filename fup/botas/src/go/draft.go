d962c058058b57e0a42ab116a10832230b22452a
levys67 <naoeolevy@gmail.com>
2026-05-25 18:18:08 +0000

    pog

---
+++ fup/botas/src/go/draft.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+type bota struct{
+    tam int
+    pe string
+}
+ 
+func ehpar( 
+
+)
+func main() {
+     
+    fmt.Println("Hello, World!")
+}
--- fup/fuga/src/go/draft.go
+++ fup/fuga/src/go/draft.go
@@ -7,17 +7,19 @@ func main() {
     f := 0
     d := 0 // -1 para horaria e 1 para anti horaria
     fmt.Scan(&h, &p, &f, &d)
-    if d < 0 && f < p {
-        for i := f; i >= 0; i--{
-            if i == h {
+    if d == 1{
+        for i := f; i >= 0 ; i--{
+            switch i {
+            case h :{
                 fmt.Println("S")
                 break
-            } else if i == p {
+            }
+            case p :{
                 fmt.Println("N")
                 break
             }
-            
-        } 
-       
+
+            }
+        }
     }
 }
